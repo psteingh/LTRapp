2a6a040ea988e6e3b1f577346dc0b531e1d7a84a
psteingh <peter3stein@gmail.com>
2023-03-19 13:18:38 -0400

    ltrapp03190118

--- home/navbottom.js
+++ home/navbottom.js
@@ -2,9 +2,16 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import Colors from '../constants/colors';
 
+import AboutUsBoard from '../boardaboutus/aboutusboard.component';
+import LierBoard from '../components/lierboard.component';
+import AboutLiesBoard from '../boardaboutlies/aboutliesboard.component';
+
 function NavBottom() {
     return (
         <View style={styles.navBottomContainer}>
+            <AboutUsBoard />
+            <LierBoard />
+            <AboutLiesBoard />
             <Text style={styles.navBottomText}> NavBottom </Text>
         </View>
     );
