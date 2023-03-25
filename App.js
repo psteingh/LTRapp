5a4646cdacedb949fe0d0ad34159a84c57dfafe0
psteingh <peter3stein@gmail.com>
2023-03-25 11:07:16 -0400

    ltrapp03251107

--- App.js
+++ App.js
@@ -2,33 +2,33 @@ import { StyleSheet, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeComponent from './components/home.component';
 import NavBottom from './home/navbottom';
 
-import AboutUsBoard from './boardaboutus/aboutusboard.component';
-import LierBoard from './components/lierboard.component';
-import AboutLiesBoard from './boardaboutlies/aboutliesboard.component';
+// import AboutUsBoard from './boardaboutus/aboutusboard.component';
+// import LierBoard from './components/lierboard.component';
+// import AboutLiesBoard from './boardaboutlies/aboutliesboard.component';
 
 import Colors from './constants/colors';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
-function NavBot() {
-    return (
-        // <NavigationContainer>
-            <Tab.Navigator>
-            <Tab.Screen name="AboutUs" component={AboutUsBoard} />
-            <Tab.Screen name="LierBoard" component={LierBoard} />
-            <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
-            </Tab.Navigator>
-    );
-    }
+// function NavBot() {
+//     return (
+//             <Tab.Navigator>
+//             <Tab.Screen name="AboutUs" component={AboutUsBoard} />
+//             <Tab.Screen name="LierBoard" component={LierBoard} />
+//             <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
+//             </Tab.Navigator>
+//     );
+//     }
 
 export default function App() {
   return (
     <NavigationContainer>
+      
       <LinearGradient
         start={{x: 0, y: 0}}
         end={{x: 1, y: 0}}
@@ -38,12 +38,13 @@ export default function App() {
       <View>
       <HomeComponent />
       </View>
-      <View>
-        <NavBot />
-      </View>
-      <View>
-        <NavBottom />
-      </View>
+      {/* <Tab.Navigator>
+            <Tab.Screen name="AboutUs" component={AboutUsBoard} />
+            <Tab.Screen name="LierBoard" component={LierBoard} />
+            <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
+      </Tab.Navigator> */}
+      {/* <NavBot /> */}
+      <NavBottom />
       </LinearGradient>
     </NavigationContainer>
   );
