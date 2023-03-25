f75b6f5bde6091e3d2578188b42ecb09f0a84f84
psteingh <peter3stein@gmail.com>
2023-03-25 10:32:29 -0400

    ltrapp03251032

--- App.js
+++ App.js
@@ -2,11 +2,29 @@ import { StyleSheet, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeComponent from './components/home.component';
 import NavBottom from './home/navbottom';
 
+import AboutUsBoard from './boardaboutus/aboutusboard.component';
+import LierBoard from './components/lierboard.component';
+import AboutLiesBoard from './boardaboutlies/aboutliesboard.component';
+
 import Colors from './constants/colors';
+
+const Tab = createBottomTabNavigator();
+
+function NavBot() {
+    return (
+        // <NavigationContainer>
+            <Tab.Navigator>
+            <Tab.Screen name="AboutUs" component={AboutUsBoard} />
+            <Tab.Screen name="LierBoard" component={LierBoard} />
+            <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
+            </Tab.Navigator>
+    );
+    }
 
 export default function App() {
   return (
@@ -19,6 +37,9 @@ export default function App() {
         >
       <View>
       <HomeComponent />
+      </View>
+      <View>
+        <NavBot />
       </View>
       <View>
         <NavBottom />
--- home/navbottom.js
+++ home/navbottom.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native';
 // import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Colors from '../constants/colors';
 
@@ -8,24 +8,24 @@ import AboutUsBoard from '../boardaboutus/aboutusboard.component';
 import LierBoard from '../components/lierboard.component';
 import AboutLiesBoard from '../boardaboutlies/aboutliesboard.component';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
 function NavBottom() {
     return (
         // <NavigationContainer>
-            <Tab.Navigator>
-            <Tab.Screen name="AboutUs" component={AboutUsBoard} />
-            <Tab.Screen name="LierBoard" component={LierBoard} />
-            <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
-            </Tab.Navigator>
+            // <Tab.Navigator>
+            // <Tab.Screen name="AboutUs" component={AboutUsBoard} />
+            // <Tab.Screen name="LierBoard" component={LierBoard} />
+            // <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
+            // </Tab.Navigator>
         // </NavigationContainer>
         
-        /* <View style={styles.navBottomContainer}>
-            <AboutUsBoard />
-            <LierBoard />
-            <AboutLiesBoard />
-            <Text style={styles.navBottomText}> NavBottom </Text>
-        </View> */
+        <View style={styles.navBottomContainer}>
+        <AboutUsBoard />
+        <LierBoard />
+        <AboutLiesBoard />
+        <Text style={styles.navBottomText}> NavBottom </Text>
+        </View>
     );
 }
 
