358e2a2ef5cdb65dfa6782b169ae3d67db696d7d
psteingh <peter3stein@gmail.com>
2023-04-01 14:02:50 -0400

    ltrapp0401203

--- App.js
+++ App.js
@@ -18,9 +18,8 @@ export default function App() {
           left: 0,
           right: 0,
           top: 0,
-          bottom: 0,
-        }}        
-        />
+          height: '100%'
+        }} />
 
       <NavBottom />
 
