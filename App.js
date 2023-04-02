784decafd80752e8ffc879906dc39ae04e8c2e83
psteingh <peter3stein@gmail.com>
2023-04-02 13:26:41 -0400

    ltrapp0402126

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StyleSheet, View, Text } from 'react-native';
 
-// import { LinearGradient } from 'expo-linear-gradient';
-
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import NavBottom from './home/navbottom';
@@ -25,23 +23,9 @@ export default function App() {
   return (
     <NavigationContainer theme={MyTheme}>
 
-      {/* <LinearGradient
-        colors={[Colors.primaryBlack, Colors.primaryWhite]}
-        style={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          height: '100%'
-        }} /> */}
-        
-        {/* start={{x: 0, y: 1}}
-        end={{x: 0, y: 0}}
-        style={styles.container} */}
-      
       <NavBottom />
 
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
