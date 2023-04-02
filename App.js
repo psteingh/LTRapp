7cbabfbdf6371b0788f65d203c720ab079acacca
psteingh <peter3stein@gmail.com>
2023-04-02 08:37:34 -0400

    ltrapp0402837

--- App.js
+++ App.js
@@ -15,9 +15,7 @@ export default function App() {
         colors={[Colors.primaryBlack, Colors.primaryWhite]}
         start={{x: 0, y: 0}}
         end={{x: 1, y: 0}}
-        style={{
-          position: "absolute",
-        }} />
+        style={{ position: "absolute" }} />
 
       {/* <NavBottom /> */}
 
