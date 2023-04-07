ca4bcaf94ff82d4f8d3b5ad3e74eec4ce36465aa
psteingh <peter3stein@gmail.com>
2023-04-07 14:20:25 -0400

    ltrapp0407220

--- App.js
+++ App.js
@@ -25,17 +25,27 @@ function MyTabs() {
 }
 
 export default function App() {
-  const navTheme = {
-  colors: { background: Colors.primaryLgreen }}
+  // const navTheme = {
+  // colors: { background: Colors.primaryLgreen }}
 
   return (
-    <NavigationContainer theme={navTheme}>
+    <NavigationContainer>
 
     <LinearGradient
+    style={{
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0
+    }}
+    colors={[Colors.primaryWhite, Colors.primaryBlack]}>
+
+    {/* <LinearGradient
     colors={[Colors.primaryWhite, Colors.primaryBlack]}
     start={{x: 0, y: 0}}
     end={{x: 1, y: 0}}
-    style={styles.container}>
+    style={styles.container}> */}
         
       <MyTabs />
 
