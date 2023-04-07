2668dfc654d0f220c4485bd39c85961cfb9e1a5f
psteingh <peter3stein@gmail.com>
2023-04-07 11:20:29 -0400

    ltrapp04071120

--- App.js
+++ App.js
@@ -11,40 +11,51 @@ import AboutUsBoard from './boardaboutus/aboutusboard.component';
 import LierBoard from './components/lierboard.component';
 import AboutLiesBoard from './boardaboutlies/aboutliesboard.component';
 
-// const Tab = createBottomTabNavigator();
+{/* <LinearGradient
+  colors={[Colors.primaryBlack, Colors.primaryWhite]}
+  start={{x: 0, y: 1}}
+  end={{x: 0, y: 0}}> */}
 
-// function MyTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Home" component={HomeComponent} />
-//       <Tab.Screen name="AboutUs" component={AboutUsBoard} />
-//       <Tab.Screen name="LierBoard" component={LierBoard} />
-//       <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
-//     </Tab.Navigator>
-//   );
-// }
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeComponent} />
+      <Tab.Screen name="AboutUs" component={AboutUsBoard} />
+      <Tab.Screen name="LierBoard" component={LierBoard} />
+      <Tab.Screen name="AboutLies" component={AboutLiesBoard} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
+  const navTheme = {
+    colors: {
+      background: Colors.primaryLgreen
+    }
+  };
 
   return (
-    // <NavigationContainer>
-      <View style={styles.container}>
-      <LinearGradient
-        colors={[Colors.primaryBlack, Colors.primaryWhite]}
-        start={{x: 0, y: 1}}
-        end={{x: 0, y: 0}} >
-
-      <Text>Start screen</Text>
-      </LinearGradient>
-      </View>
-    // </NavigationContainer>
+    <NavigationContainer theme={navTheme}>
+        
+      <MyTabs />
+      
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    // alignItems: 'center',
+    // justifyContent: 'center'
   },
+  tabColor: {
+    background: Colors.primaryDgreen
+  },
+  linGrad: {
+    width: '100%',
+    height: '100%'
+  }
 });
