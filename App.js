2ec4468d39052e1b485ddd919854627ee202807f
psteingh <peter3stein@gmail.com>
2023-04-08 09:37:23 -0400

    ltrapp0408937

--- App.js
+++ App.js
@@ -25,25 +25,23 @@ function MyTabs() {
 }
 
 export default function App() {
-  // const navTheme = {
-  // colors: { background: Colors.primaryLgreen }}
+  const navTheme = {
+  colors: { background: Colors.primaryLgreen }}
 
   return (
-    
-    
+    <NavigationContainer theme={navTheme}>
+
     <LinearGradient
     colors={[Colors.primaryWhite, Colors.primaryBlack]}
     start={{x: 0, y: 0}}
     end={{x: 1, y: 0}}
     style={styles.container}>
 
-    <NavigationContainer>
-        
       <MyTabs />
 
-    </NavigationContainer>
-    
     </LinearGradient>
+
+    </NavigationContainer>
     
   );
 }
