1990eb178f4589eb441be486538dc2a9491c0519
psteingh <peter3stein@gmail.com>
2023-04-08 08:38:43 -0400

    ltrapp0408839

--- App.js
+++ App.js
@@ -51,11 +51,7 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center'
   },
-  // tabColor: {
-  //   background: Colors.primaryDgreen
-  // },
-  // linGrad: {
-  //   width: '100%',
-  //   height: '100%'
-  // }
+  contTheme: {
+    background: Colors.primaryDgreen
+  }
 });
