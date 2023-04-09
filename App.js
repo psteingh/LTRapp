68ea5b03c8a7994619e1351d4619f3a8140db0f8
psteingh <peter3stein@gmail.com>
2023-04-09 11:37:01 -0400

    ltrapp04091137

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { LinearGradient } from 'expo-linear-gradient';
@@ -13,22 +13,6 @@ import AboutLiesBoard from './boardaboutlies/aboutliesboard.component';
 
 const Tab = createBottomTabNavigator();
 
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: Colors.primaryLred,
-    background: Colors.primaryLgreen,
-    card: Colors.primaryLblue
-    // background: () => (
-    // <LinearGradient
-    // colors={[Colors.primaryWhite, Colors.primaryBlack]}
-    // start={{x: 0, y: 0}}
-    // end={{x: 1, y: 0}}
-    // style={styles.container} />
-    // ),
-  }, };
-
 {/* <LinearGradient
     colors={[Colors.primaryWhite, Colors.primaryBlack]}
     start={{x: 0, y: 0}}
@@ -40,7 +24,14 @@ function MyTabs() {
   return (
     <Tab.Navigator
     screenOptions={{
-      headerStyle: {backgroundColor: Colors.primaryLblue},
+      headerStyle: {backgroundColor: () => (
+        <LinearGradient
+        colors={[Colors.primaryWhite, Colors.primaryBlack]}
+        start={{x: 1, y: 0}}
+        end={{x: 0, y: 0}}
+        style={styles.container}
+        />
+      ),},
       tabBarBackground: () => (
         <LinearGradient
         colors={[Colors.primaryWhite, Colors.primaryBlack]}
@@ -62,12 +53,6 @@ export default function App() {
   
   return (
     <NavigationContainer>
-    
-    <LinearGradient
-        colors={[Colors.primaryWhite, Colors.primaryBlack]}
-        start={{x: 0, y: 0}}
-        end={{x: 1, y: 0}}
-        style={styles.container}/>
     
     <MyTabs />
 
