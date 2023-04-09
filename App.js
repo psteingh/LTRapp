fb26df357cea07e520050d8c92c8596a5fe7c92e
psteingh <peter3stein@gmail.com>
2023-04-09 08:27:31 -0400

    ltrapp0409827

--- App.js
+++ App.js
@@ -59,7 +59,12 @@ function MyTabs() {
 export default function App() {
   
   return (
-    <NavigationContainer theme={MyTheme}>
+    <NavigationContainer>
+    <LinearGradient
+        colors={[Colors.primaryWhite, Colors.primaryBlack]}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        style={styles.container} />
     
     <MyTabs />
 
