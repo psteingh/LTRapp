e08ea0d0679df60240c3ba8884b871ab93b175f0
psteingh <peter3stein@gmail.com>
2023-04-15 13:02:12 -0400

    ltrapp0415102

--- constants/lingrad.js
+++ constants/lingrad.js
@@ -3,13 +3,16 @@ import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Colors from '../constants/colors';
 
-function LinGrad() {
+function LinGrad({children}) {
   return (
   <LinearGradient
   colors={[Colors.primaryWhite, Colors.primaryBlack]}
   start={{x: 0, y: 0}}
   end={{x: 1, y: 0}}
-  style={styles.container}/>
+  style={styles.container}
+  >
+  {children}
+  </LinearGradient>
   );
 }
 
