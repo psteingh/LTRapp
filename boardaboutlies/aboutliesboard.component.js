58e5519ae2da5dfbfb33a75bcfb1ae56d57eb890
psteingh <peter3stein@gmail.com>
2023-04-22 13:04:07 -0400

    ltrapp0422104

--- boardaboutlies/aboutliesboard.component.js
+++ boardaboutlies/aboutliesboard.component.js
@@ -1,12 +1,15 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 import Colors from '../constants/colors';
+import LinGrad from '../constants/lingrad';
 
 function AboutLiesBoard() {
     return (
+        <LinGrad>
         <View>
             <Text style={styles.aboutLiesBoardText}> About Lies Board </Text>
         </View>
+        </LinGrad>
     );
 }
 
