a59c704ab2e4ede5f4d2ddbeffe95294a1d642d8
psteingh <peter3stein@gmail.com>
2023-04-23 13:23:24 -0400

    ltrapp0423123

--- boardaboutlies/aboutliesboard.component.js
+++ boardaboutlies/aboutliesboard.component.js
@@ -1,25 +1,48 @@
 import { View, Text, StyleSheet } from 'react-native';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 import Colors from '../constants/colors';
-import LinGradScreen from '../constants/lingradscreen';
+// import LinGradScreen from '../constants/lingradscreen';
+
+{/* <LinGradScreen>
+    <View style={styles.viewContainer}>
+    <Text style={styles.aboutLiesBoardText}> About Lies Board </Text>
+    </View>
+</LinGradScreen> */}
 
 function AboutLiesBoard() {
     return (
-        <LinGradScreen>
-        <View style={styles.viewContainer}>
-            <Text style={styles.aboutLiesBoardText}> About Lies Board </Text>
+        <View style={styles.container}>
+        <LinearGradient
+            colors={['rgba(0,0,0,0.8)', 'transparent']}
+            style={styles.background}
+             />
+        <Text style={styles.aboutLiesBoardText}>
+            About Lies Board
+        </Text>
+
         </View>
-        </LinGradScreen>
     );
 }
 
 export default AboutLiesBoard;
 
 const styles = StyleSheet.create({
-    viewContainer: {
-        flex: 1
+    container: {
+        flex: 1,
+        backgroundColor: 'orange'
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: 300
     },
     aboutLiesBoardText: {
-        color: Colors.primaryLred
+        fontSize: 20,
+        color: Colors.primaryLred,
+        backgroundColor: 'transparent'        
     }
 });
