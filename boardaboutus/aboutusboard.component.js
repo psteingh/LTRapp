6b9dc14798b6edab290bb75a96102a670abdbccb
psteingh <peter3stein@gmail.com>
2023-04-30 08:28:55 -0400

    ltrapp0430829

--- boardaboutus/aboutusboard.component.js
+++ boardaboutus/aboutusboard.component.js
@@ -1,6 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-import Colors from '../constants/colors';
 import LinGradScreen from '../constants/lingradscreen';
 import TitleText from '../constants/titletext';
 
@@ -8,8 +7,7 @@ function AboutUsBoard() {
     return (
         <View style={styles.container}>
         <LinGradScreen />
-        <TitleText>Test TitleText component</TitleText>
-            <Text style={styles.aboutUsBoardText}> About Us Board </Text>
+        <TitleText>About Us Board</TitleText>
         </View>
     );
 }
@@ -19,10 +17,5 @@ export default AboutUsBoard;
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    },
-    aboutUsBoardText: {
-        fontSize: 20,
-        color: Colors.primaryLred,
-        backgroundColor: 'transparent'
     }
 });
