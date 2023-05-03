3aa4abb8513e947b1f5186e88011ef36dcd79026
psteingh <peter3stein@gmail.com>
2023-05-03 12:12:26 -0400

    ltrapp05031213

--- components/home.component.js
+++ components/home.component.js
@@ -1,7 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 
 import LinGradScreen from '../constants/lingradscreen';
-import TitleText from '../constants/titletext';
+// import TitleText from '../constants/titletext';
 
 import TopHead from '../home/tophead';
 import HomeMid from '../home/homemid';
@@ -11,7 +11,7 @@ function HomeComponent() {
     return (
         <View style={styles.homeContainer}>
         <LinGradScreen />
-        <TitleText>Home</TitleText>
+        {/* <TitleText>Home</TitleText> */}
         
         <TopHead />
         <HomeMid />
