ece355fc8c0f80bcd99a7de43b271f7b22621c9b
psteingh <peter3stein@gmail.com>
2023-05-04 12:19:18 -0400

    ltrapp05041219

---
+++ home/contact.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+import Colors from '../constants/colors';
+
+function Contact() {
+    return (
+        <View style={styles.contactContainer}>
+            <Text style={styles.contactText}>Contact Us</Text>
+        </View>
+    );
+}
+
+export default Contact;
+
+const styles = StyleSheet.create({
+    contactContainer: {
+        backgroundColor: Colors.primaryLgreen,
+        margin: 5,
+        borderWidth: 1,
+        borderColor: Colors.primaryDgreen
+    },
+    contactText: {
+        fontFamily: 'RobotoBold',
+        fontSize: 10,
+        color: Colors.primaryWhite,
+        margin: 5
+    }
+});
--- home/footer.js
+++ home/footer.js
@@ -4,8 +4,16 @@ import Colors from '../constants/colors';
 
 function Footer() {
     return (
-        <View>
-            <Text style={styles.footerText}> Footer </Text>
+        <View style={styles.footerContainer}>
+            <View style={styles.footerChildContainer}>
+            <Text style={styles.footerText}>Note</Text>
+            </View>
+            <View style={styles.footerChildContainer}>
+            <Text style={styles.footerText}>LTR logo</Text>
+            </View>
+            <View style={styles.footerChildContainer}>
+            <Text style={styles.footerText}>Contact Us</Text>
+            </View>
         </View>
     );
 }
@@ -13,6 +21,19 @@ function Footer() {
 export default Footer;
 
 const styles = StyleSheet.create({
+    footerContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        margin: 5,
+        borderWidth: 1,
+        borderColor: Colors.primaryDblue
+    },
+    footerChildContainer: {
+        flex: 2,
+        borderWidth: 1,
+        borderColor: Colors.primaryLred,
+        alignItems: 'center'
+    },
     footerText: {
         color: Colors.primaryDred
     }
