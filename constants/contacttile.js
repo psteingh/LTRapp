ce00e715e54270abfb186ab92c72136f03d42222
psteingh <peter3stein@gmail.com>
2023-05-07 10:21:42 -0400

    ltrapp05071022

---
+++ constants/contacttile.js
@@ -0,0 +1,44 @@
+import { Pressable, View, Text, StyleSheet } from 'react-native';
+
+function ContactTile({title, color, onPress}) {
+    return (
+        <View style={styles.gridItem}>
+        <Pressable
+            style={({ pressed }) => [styles.button,
+                pressed ? styles.buttonPressed : null,]}
+                onPress={onPress}>
+            <View style={[styles.innerContainer, {backgroundColor: color}]}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        </Pressable>
+        </View>
+    );
+}
+
+export default ContactTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        elevation: 4,
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        flex: 1
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+});
