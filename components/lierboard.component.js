7041f31f05eb6468471d6432bc22b7a1f2a01fa6
psteingh <peter3stein@gmail.com>
2023-05-09 10:14:53 -0400

    ltrapp05091015

--- components/lierboard.component.js
+++ components/lierboard.component.js
@@ -1,13 +1,19 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 
 import LinGradScreen from '../constants/lingradscreen';
 import TitleText from '../constants/titletext';
 
-function LierBoard() {
+function LierBoard({navigation}) {
     return (
         <View style={styles.container}>
         <LinGradScreen />
         <TitleText>Lier Board</TitleText>
+        <Button
+            title="Go to Contact Page"
+            onPress={() => navigation.navigate('ContactPage')} />
+        <Button
+            title="Go to About Lies"
+            onPress={() => navigation.navigate("AboutLies")} />
         </View>
     );
 }
