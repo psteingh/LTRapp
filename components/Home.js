9bf2db78b099cad3cd91404b77757319af9a7fad
psteingh <peter3stein@gmail.com>
2023-05-11 12:49:29 -0400

    ltrapp05111250

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,6 @@
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import Colors from '../constants/colors';
 
 import LinGradScreen from '../constants/lingradscreen';
 
@@ -15,10 +17,12 @@ import Footer from '../home/Footer';
         <HomeMid />
         <Footer />
 
+        <View style={styles.contactButtonContainer}>
+        <Text style={styles.contactButtonText}>Contact Us</Text>
         <Button
-            title="MT CP"
-            onPress={() => navigation.navigate('MyTabs',
-            { screen: 'ContactPage' }) } />
+            title="Go to Contact Page"
+            onPress={() => navigation.navigate('MyTabs', {screen: 'ContactPage'})} />
+        </View>
 
         </View>
     );
@@ -29,5 +33,17 @@ export default Home;
 const styles = StyleSheet.create({
     homeContainer: {
         flex: 1
+    },
+    contactButtonContainer: {
+        backgroundColor: Colors.primaryLgreen,
+        margin: 5,
+        borderWidth: 1,
+        borderColor: Colors.primaryDgreen
+    },
+    contactButtonText: {
+        fontFamily: 'Roboto',
+        fontSize: 10,
+        color: Colors.primaryWhite,
+        margin: 5
     }
 });
