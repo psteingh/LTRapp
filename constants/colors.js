b1f866b6d893b4e3699508e272d0026208dfc39f
psteingh <peter3stein@gmail.com>
2023-05-13 11:42:06 -0400

    ltrapp05131142

--- constants/colors.js
+++ constants/colors.js
@@ -2,6 +2,7 @@ const Colors = {
     primaryBlack: '#000000',
     primaryWhite: '#FFFFFF',
     primaryOffWhite: '#FFFFF0',
+    primaryYellow: '#E9EC47',
     primaryDblue: '#36648B',
     primaryLblue: '#63B8FF',
     primaryDgreen: '#368B88',
