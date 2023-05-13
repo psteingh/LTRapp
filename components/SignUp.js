4e1166f88e33ed2e7760d2fb6ee29613f9cb3538
psteingh <peter3stein@gmail.com>
2023-05-13 08:45:19 -0400

    ltrapp0513846

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,13 +1,22 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+import Colors from '../constants/colors';
 
 import LinGradScreen from '../constants/LinGradScreen';
 import TitleText from '../constants/TitleText';
 
-  function SignUp() {
+  function SignUp({navigation}) {
     return (
         <View style={styles.signUpContainer}>
         <LinGradScreen />
         <TitleText>Sign Up</TitleText>
+        <View style={styles.signUpPressableContainer}>
+        <Pressable
+            style={styles.signUpButtonContainer}
+            onPress={() => navigation.navigate("LogIn")} >
+        <Text style={styles.signUpButtonText}>Login Page</Text>
+        </Pressable>
+        </View>
         </View>
     );
 }
@@ -17,5 +26,20 @@ export default SignUp;
 const styles = StyleSheet.create({
     signUpContainer: {
         flex: 1
+    },
+    signUpPressableContainer: {
+        backgroundColor: Colors.primaryDblue,
+        alignItems: 'center',
+        margin: 5
+    },
+    signUpButtonContainer: {
+        backgroundColor: Colors.primaryLgreen,
+        margin: 5
+    },
+    signUpButtonText: {
+        fontFamily: 'Roboto',
+        fontSize: 10,
+        color: Colors.primaryWhite,
+        margin: 5
     }
 });
