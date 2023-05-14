cd298ae0174101b8bcdc8f48c91a8319eb21536b
psteingh <peter3stein@gmail.com>
2023-05-14 09:00:05 -0400

    ltrapp0514900

--- constants/GoToButton.js
+++ constants/GoToButton.js
@@ -1,13 +1,23 @@
-import { Button } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
+
+import Colors from './colors';
 
 function GoToButton({screenName}) {
     const navigation = useNavigation();
     return (
         <Button title={`Go to ${screenName}`}
-        onPress={()=> navigation.navigate(screenName)} />
+        onPress={()=> navigation.navigate(screenName)}
+        style={styles.goToButtonContainer} />
     );
 }
 
 export default GoToButton;
+
+const styles = StyleSheet.create({
+    goToButtonContainer: {
+        margin: 5,
+        backgroundColor: Colors.primaryLgreen
+    }
+});
