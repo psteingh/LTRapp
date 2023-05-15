02e79b431d21073abce8dbd3cc20b1e2078b1e1a
psteingh <peter3stein@gmail.com>
2023-05-15 13:14:38 -0400

    ltrapp0515115

--- home/ButtonUseNav.js
+++ home/ButtonUseNav.js
@@ -28,9 +28,7 @@ const styles = StyleSheet.create({
     },
     contactButtonContainer: {
         backgroundColor: Colors.primaryLgreen,
-        margin: 5,
-        borderWidth: 1,
-        borderColor: Colors.primaryDgreen
+        margin: 5
     },
     contactButtonText: {
         fontFamily: 'Roboto',
