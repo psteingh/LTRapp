53d3822d18367973d9ad9c289053d45659442a76
psteingh <peter3stein@gmail.com>
2023-05-20 12:03:51 -0400

    ltrapp05201204

--- constants/HomeMidUseNav.js
+++ constants/HomeMidUseNav.js
@@ -7,7 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 function HomeMidUseNav({screenName, children}) {
     const navigation = useNavigation();
     return (
-        <View style={styles.homeMidUseNavCont}>
+        <View>
         <Pressable
             onPress={() => navigation.navigate(screenName)}>
         <View style={styles.homeMidUseNavChildCont}>{children}</View>
@@ -21,12 +21,12 @@ export default HomeMidUseNav;
 const styles = StyleSheet.create({
     homeMidUseNavCont: {
         alignItems: 'center',
-        borderWidth: 1,
+        borderWidth: 2,
         borderColor: Colors.primaryLblue
     },
     homeMidUseNavChildCont: {
         alignItems: 'center',
         borderWidth: 1,
-        borderColor: Colors.primaryLgreen
+        borderColor: Colors.primaryYellow
     }
 });
