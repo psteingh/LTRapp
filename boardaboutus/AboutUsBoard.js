dcddecea97313662852a467f5a401bcc3e834917
psteingh <peter3stein@gmail.com>
2023-05-27 13:10:21 -0400

    ltrapp0527111

--- boardaboutus/AboutUsBoard.js
+++ boardaboutus/AboutUsBoard.js
@@ -3,11 +3,14 @@ import { View, StyleSheet } from 'react-native';
 import LinGradScreen from '../constants/LinGradScreen';
 import TitleText from '../constants/TitleText';
 
+import AboutUsImage from './AboutUsImage';
+
 function AboutUsBoard() {
     return (
         <View style={styles.aboutUsBoardContainer}>
         <LinGradScreen />
         <TitleText>About Us Board</TitleText>
+        <AboutUsImage />
         </View>
     );
 }
---
+++ boardaboutus/AboutUsImage.js
@@ -0,0 +1,25 @@
+import { View, Image, StyleSheet } from 'react-native';
+
+import TextImage from '../constants/TextImage';
+
+function AboutUsImage() {
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.aboutUsImage}
+        source={require('../assets/images/ltrdeceive.png')} />
+      <TextImage>About Us</TextImage>
+    </View>
+  );
+}
+
+export default AboutUsImage;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    position: 'relative'
+  },
+  aboutUsImage: {
+    width: 5
+  }
+});
