6ab6708d8fb7b9c6cf39660baee33ab1bae14998
psteingh <peter3stein@gmail.com>
2023-05-28 13:48:53 -0400

    ltrapp0528149

--- App.js
+++ App.js
@@ -23,7 +23,10 @@ export default function App() {
     <>
     <StatusBar style={styles.statusBarContainer} />
     <NavigationContainer>
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: 'tomato'}
+      }}>
       <Stack.Screen
         name="HomeBottomTab"
         component={HomeBottomTab}
