47d9f4feb6654c6ec002efb72da4e76b69b815d5
psteingh <peter3stein@gmail.com>
2023-06-02 12:29:10 -0400

    ltrapp0602231229

--- constants/TextImage.js
+++ constants/TextImage.js
@@ -19,6 +19,7 @@ const styles = StyleSheet.create({
         right: 0,
         top: 0,
         bottom: 0,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     textImageFont: {
