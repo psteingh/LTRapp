2dfc8dbcadffbb91fdeb94ae03460e323415e39f
psteingh <peter3stein@gmail.com>
2023-06-02 12:37:52 -0400

    ltrapp0602231237

--- constants/TextImage.js
+++ constants/TextImage.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     },
     textImageFont: {
         fontFamily: 'RobotoBold',
-        fontSize: 40,
+        fontSize: 30,
         color: Colors.primaryOffWhite,
     },
 });
