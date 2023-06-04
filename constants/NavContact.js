c3fd76a9b5f009ca02ffb80e3e1bc8bff21af487
psteingh <peter3stein@gmail.com>
2023-06-04 12:52:49 -0400

    ltrapp06041252

--- constants/NavContact.js
+++ constants/NavContact.js
@@ -4,14 +4,14 @@ import { useNavigation } from '@react-navigation/native';
 
 import Colors from './colors';
 
-function NavContact({screenName}) {
+function NavContact({screenName, screenTitle }) {
     const navigation = useNavigation();
     return (
         <View>
         <Pressable
             onPress={() => navigation.navigate(screenName)}
             style={styles.navContactContainer}>
-        <Text style={styles.navContactText}>{`${screenName}`}</Text>
+        <Text style={styles.navContactText}>{`${screenTitle}`}</Text>
         </Pressable>
         </View>
     );
