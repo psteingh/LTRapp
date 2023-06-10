235e2795dae76e29f46b5a8fe5f61619f154c008
psteingh <peter3stein@gmail.com>
2023-06-10 10:16:38 -0400

    ltrapp06101016

--- home/CarouselTest.js
+++ home/CarouselTest.js
@@ -1,28 +1,22 @@
 import * as React from 'react';
-import { Dimensions, Text, View } from 'react-native';
+import { Dimensions, Text, View, StyleSheet } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 
 function CarouselTest() {
     const width = Dimensions.get('window').width;
     return (
-        <View style={{ flex: 1 }}>
+        <View style={styles.carouselContainer}>
             <Carousel
                 loop
                 width={width}
                 height={width / 2}
                 autoPlay={true}
-                data={[...new Array(6).keys()]}
+                data={[...new Array(3).keys()]}
                 scrollAnimationDuration={1000}
                 onSnapToItem={(index) => console.log('current index:', index)}
                 renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                    <View style={styles.carouselTextContainer}>
+                        <Text style={styles.carouselText}>
                             {index}
                         </Text>
                     </View>
@@ -33,3 +27,21 @@ function CarouselTest() {
 }
 
 export default CarouselTest;
+
+const styles = StyleSheet.create({
+    carouselContainer: {
+        flex: 1
+    },
+    carouselTextContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: 'yellow'
+    },
+    carouselText: {
+        margin: 5,
+        textAlign: 'center',
+        fontSize: 10,
+        color: 'white'
+    }
+});
