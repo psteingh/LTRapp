fc5e276a28cbb54f2c5b9bb0dd74d89a4370fd8d
psteingh <peter3stein@gmail.com>
2023-06-11 13:45:50 -0400

    ltrapp0611145

--- home/CarouselBS.js
+++ home/CarouselBS.js
@@ -4,8 +4,21 @@ import Carousel from 'react-native-reanimated-carousel';
 
 import Colors from '../constants/colors';
 
+class Category{
+    constructor(id, title){
+        this.id = id;
+        this.title = title;
+    }}
+
 function CarouselBS() {
     const width = Dimensions.get('window').width;
+
+    const CATEGORIES = [
+        new Category('1', 'When'),
+        new Category('2', 'Did'),
+        new Category('3', 'What'),
+        new Category('4', 'Why'),
+    ];
     
     return (
         <View style={styles.carouselContainer}>
@@ -17,13 +30,16 @@ function CarouselBS() {
             data={[...new Array(4).keys()]}
             scrollAnimationDuration={3000}
 
-            onSnapToItem={(index) => console.log(index)}
+            onSnapToItem={(index) => console.log(index, CATEGORIES)}
             
             renderItem={({index}) => (
+            <View>
+            <View style={styles.carouselTextContainer}>            
+            <Text style={styles.carouselText}>{index}</Text>
+            </View>
             <View style={styles.carouselTextContainer}>
-            
-            <Text style={styles.carouselText}>{index}When</Text>
-            <Text style={styles.carouselText}>{index}What</Text>
+            <Text style={styles.carouselText}></Text>
+            </View>
 
             {/* <Text style={styles.carouselText}>
                 When did I tell them a Lie?</Text>
