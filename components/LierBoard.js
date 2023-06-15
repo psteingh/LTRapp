9f0e137347d38763599017977aeeb444dccdd0ed
psteingh <peter3stein@gmail.com>
2023-06-15 12:54:14 -0400

    ltrapp0615231254

--- components/LierBoard.js
+++ components/LierBoard.js
@@ -1,15 +1,14 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import LinGradScreen from '../constants/LinGradScreen';
-import TextTitle from '../constants/TextTitle';
-
+import Colors from '../constants/colors';
 import NavLtrLogo from '../constants/NavLtrLogo';
 
 function LierBoard() {
     return (
         <View style={styles.lierBoardContainer}>
         <LinGradScreen />
-        <TextTitle>Lier Board</TextTitle>
+        <Text style={styles.lierBoardTitleText}>Lies made Public</Text>
         <View style={styles.navLtrLogoContainer}>
         <NavLtrLogo screenName='Home' />
         </View>
@@ -22,6 +21,11 @@ export default LierBoard;
 const styles = StyleSheet.create({
     lierBoardContainer: {
         flex: 1
+    },
+    lierBoardTitleText: {
+        color: Colors.primaryDgreen,
+        fontSize: 15,
+        fontFamily: 'LibreBaskervilleBold'
     },
     navLtrLogoContainer: {
         alignItems: 'center'
