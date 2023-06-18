d75e69ea29d3c8e6c2edef3325bec6648cdfde49
psteingh <peter3stein@gmail.com>
2023-06-18 09:53:57 -0400

    ltrapp0618953

--- home/footer.js
+++ home/footer.js
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
 
 import Colors from '../constants/colors';
 
@@ -8,7 +9,13 @@ import NavContact from '../constants/NavContact';
 
 function Footer() {
     return (
-        <View style={styles.footerContainer}>
+        <View style={styles.footerShadowContainer}>
+        <Shadow
+            distance={2}
+            startColor={'#7f7f7f'}
+            offset={[2, 2]}
+            style={styles.shadowCorner}>
+        <View style={styles.footerShadowChildContainer}>
             <View>
             <FooterNote />
             </View>
@@ -21,15 +28,22 @@ function Footer() {
                 screenTitle='Contact Us' />
             </View>
         </View>
+        </Shadow>
+        </View>
     );
 }
 
 export default Footer;
 
 const styles = StyleSheet.create({
-    footerContainer: {
-        backgroundColor: Colors.primaryDblue,
+    footerShadowContainer: {
         alignItems: 'center',
         margin: 5
+    },
+    shadowCorner: {
+        borderRadius: 1
+    },
+    footerShadowChildContainer: {
+        backgroundColor: Colors.primaryDblue,
     }
 });
